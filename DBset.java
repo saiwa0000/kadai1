25751463ac60c65dd732d553f43969ee9dd9086c
Haruto NIWA <cnt14095@cs-t151.cs.nitech.ac.jp>
2025-05-23 17:04:09 +0900

    データセット更新

--- DBset.java
+++ DBset.java
@@ -3,11 +3,22 @@ import java.util.*;
 
 public class DBset {
     public static void main(String args[]){
-        List<WordList> wlist = WordListDAO.findAll(); 
+        List<WordList> wl = WordListDAO.findAll(); 
 
-        wlist.add( new WordList(1, "apple", "りんご", 0));
+        wl.add(new WordList(1, "apple", "りんご", 0));
+        wl.add(new WordList(2, "banana", "バナナ", 0));
+        wl.add(new WordList(3, "grape", "ぶどう", 0));
+        wl.add(new WordList(4, "dog", "いぬ", 0));
+        wl.add(new WordList(5, "cat", "ねっこ", 0));
+        wl.add(new WordList(6, "bird", "とり", 0));
+        wl.add(new WordList(7, "sad", "かなしい", 0));
+        wl.add(new WordList(8, "happy", "しあわせな", 0));
+        wl.add(new WordList(9, "angry", "おこった", 0));
+        wl.add(new WordList(10, "joy", "だのしい", 0));
+        wl.add(new WordList(11, "play", "あそぶ", 0));
+        wl.add(new WordList(12, "study", "勉強する", 0));
 
-        WordListDAO.SaveAll(wlist);
+        WordListDAO.SaveAll(wl);
     }
 
     
